dbf31e7d5ce1bf6b374ef5f7f855dc88ee3eab59
sulaman <Sulamanshahzad492@gmail.com>
2026-08-26 23:55:31 +0500

    connecting mongodb

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
+import { connectDB } from "./utils/db.js"
 
 const app=express()
+connectDB()
 
 app.get("/",(req,res)=>{
     res.send("hello world")
--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+
+configDotenv()
+const mongoDB_URI=process.env.mongoDB_URI
+
+export const connectDB=async () => {
+    try {
+        await mongoose.connect(mongoDB_URI)
+        console.log("mongodb connected succesfully");
+    } catch (error) {
+        console.log("error connecting mongodb",error);
+    }
+}
