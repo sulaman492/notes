4ad9f60bff27145d1c88170d02cc4330202437bf
sulaman <Sulamanshahzad492@gmail.com>
2026-08-29 22:50:34 +0500

    defining notes schema

---
+++ server/src/models/note.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const noteSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+        required:true,
+    }
+},{timestamps:true})
+
+export const notesModel=mongoose.model("notes",noteSchema)
