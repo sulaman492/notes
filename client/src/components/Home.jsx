f0d4e80badb373d8a913bcc2a8371c5cb26d5515
sulaman <Sulamanshahzad492@gmail.com>
2026-09-03 01:20:22 +0500

    add delete note functionality

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
         if (rightSideShowingAllNotes) {
             setRightSideShowingAllNotes(false)
         }
-        else{
+        else {
             setRightSideShowingAllNotes(true)
         }
     }
@@ -39,20 +39,30 @@ const Home = () => {
         setContent(e.target.value)
     }
 
-    const handleNoteSubmit=async (e)=>{
+    const handleNoteSubmit = async (e) => {
         e.preventDefault()
         try {
-            const response=await instance.post("/createNote",{
-                title:title,
-                content:content
+            const response = await instance.post("/createNote", {
+                title: title,
+                content: content
             })
             console.log(response.data);
-            
+
         } catch (error) {
-            console.log("error while create note",error);
+            console.log("error while create note", error);
         }
         getAllNote()
         handleCreateButton()
+    }
+
+    const handleDeleteNote=async (id) => {
+        try {
+            const response=await instance.delete(`/deleteNote/${id}`)
+            console.log(response.data);
+        } catch (error) {
+            console.log("error deleting note ",error)
+        }
+        getAllNote()
     }
 
     return (
@@ -65,12 +75,16 @@ const Home = () => {
                     </button>
                 </div>
                 {/* right side */}
-                {rightSideShowingAllNotes ? <div className='right w-full h-screen border  border-red-900'>
+                {rightSideShowingAllNotes ? <div className='right w-full h-screen border  border-red-900 flex gap-2'>
                     {
                         note.map((note) => {
-                            return <div key={note._id} className='flex flex-col'>
+                            return <div key={note._id} className='flex flex-col '>
                                 <h3>{note.title}</h3>
                                 <p>{note.content}</p>
+                                <div className='flex gap-2 m-2'>
+                                    <button className='cursor-pointer bg-green-400 rounded-md p-1 font-semi-bold' >edit</button>
+                                    <button className='cursor-pointer bg-red-400 rounded-md p-1 font-semi-bold' onClick={()=>(handleDeleteNote(note._id))}>delete</button>
+                                </div>
                             </div>
                         })
                     }
@@ -80,10 +94,10 @@ const Home = () => {
                             <input className='border border-amber-400' value={title} onChange={handleTitleChange} />
 
                             <textarea className="border border-amber-400" value={content} onChange={handleContentChange} >
-                                
+
                             </textarea>
-                        <button type='submit'>save</button>
-                        <button onClick={handleCreateButton}>cancel</button>
+                            <button type='submit'>save</button>
+                            <button onClick={handleCreateButton}>cancel</button>
                         </form>
                     </div>
                 }
